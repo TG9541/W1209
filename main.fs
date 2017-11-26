376aabcb65cc20a8dc15a5951162fa3f0b4edb5f
Thomas <thomas.goeppel@gmail.com>
2017-11-26 10:55:01 +0100

    global dependencies for logger

--- main.fs
+++ main.fs
@@ -10,6 +10,7 @@ RAM
   $4002 CONSTANT EE.HYS
   $4004 CONSTANT EE.COR
   $4006 CONSTANT EE.DEL
+  $4008 CONSTANT EE.LOG
 
   $8000 CONSTANT DEFAULT
 
@@ -17,6 +18,22 @@ TARGET
 
   : init ( -- )
     \ chained init - starting point
+  ;
+
+  : .0 ( n -- )
+    \ print fixed point number
+    DUP DEFAULT = IF
+      DROP ."  DEF."  \ default display, e.g. sensor error
+    ELSE
+      \ formatted output for 3 digit 10x fixed point numbers
+      DUP -99 < OVER 999 SWAP < OR IF
+        \ number (and sign) too big for 3 7S-LED digits
+        5 + 10 / . \ +0.5 before "floor division"
+      ELSE
+        \ -9.9 <= val <= 99.9
+        SPACE DUP >R ABS <# # 46 HOLD #S R> SIGN #> TYPE
+      THEN
+    THEN
   ;
 
 #include measure.fs
